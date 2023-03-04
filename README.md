d07f4d2ea847ffff6eb091267e411ba7da6da962
ramses2099 <ramses2099@gmail.com>
2023-03-04 10:33:03 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pythonfundamentals
 Python Fundamentals Project for test python code
+
+# install extension vscode
+Code runner
+Python extnsion
+
+# install pip in and update for python version 3.10
+python -m ensurepip
+python -m pip install --upgrade pip
+python.exe -m pip install -U autopep8
---
+++ main.py
@@ -0,0 +1,18 @@
+# create list
+performances = ['Ventriloquism', 'Amazing Acrobatics']
+performances.append('Snake Charmer')
+prices = [2.50, 3.50, 4.50]
+
+# create dictionary
+slang = {'cheerio': 'goodbye', 'knackered': 'tired', 'yonks': 'ages'}
+
+print(slang['cheerio'])
+
+
+def test():
+    for price in prices:
+        print(price)
+
+
+if __name__ == '__main__':
+    test()
